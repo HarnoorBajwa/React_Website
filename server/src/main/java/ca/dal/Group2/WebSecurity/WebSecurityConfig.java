67e6dcca65fb13a9945f6c8f27b8418f4b15a62a
W <w@DF0D5.VPN.Dal.Ca>
2022-07-03 22:58:11 -0300

    Fixing web security issues

--- server/src/main/java/ca/dal/Group2/WebSecurity/WebSecurityConfig.java
+++ server/src/main/java/ca/dal/Group2/WebSecurity/WebSecurityConfig.java
@@ -1,18 +1,20 @@
+
 package ca.dal.Group2.WebSecurity;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/**").permitAll();
+        return http.build();
     }
 }
+
