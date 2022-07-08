f14dcd060bf95506529c3bbb95a287dd72e48e38
ellisk <el980442@dal.ca>
2022-07-08 18:59:14 -0300

    can now add boards again

--- server/src/main/java/Group2/entity/BoardEntity.java
+++ server/src/main/java/Group2/entity/BoardEntity.java
@@ -1,5 +1,7 @@
 package Group2.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import lombok.Data;
@@ -8,6 +10,7 @@ import lombok.Data;
 @TableName(value = "board")
 public class BoardEntity {
 
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
     private Long workSpaceId;
     private String boardName;
