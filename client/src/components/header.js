d84510a52428c2ad9333417f441e05adefc7bd63
ellisk <el980442@dal.ca>
2022-07-22 15:23:52 -0300

    now need to login to access and other access to view pages

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -7,13 +7,16 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import CreateWorkspaceButton from "./CreateWorkspaceButton";
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 
 
 function Header() {
+  const history = useHistory();
   const logOut = () => {
     localStorage.setItem("user","");
+    localStorage.setItem("board","");
+    history.push("login");
   }
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -22,7 +25,7 @@ function Header() {
       <button><Link to="/forgot-password">Forgot password</Link></button>
       <button><Link to="/workspace">Workspace and Boards</Link></button>
       <button><Link to="/tasks">Tasks</Link></button>
-      <button><Link to="/home">Good Home Page</Link></button>
+      {/* <button><Link to="/home">Good Home Page</Link></button> */}
       <button onClick={logOut}>Logout</button>
       <AppBar position="static">
         <Toolbar variant="dense">
